037878bb872166155811bb015ee1962bd3d0863f
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-02-10 22:51:31 +0100

    Add src/screens/Home.styles.ts

---
+++ src/screens/Home.styles.ts
@@ -0,0 +1,50 @@
+
+
+```
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  // Layout
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+
+  // Colors
+  primary: {
+    color: '#000',
+  },
+  secondary: {
+    color: '#808080',
+  },
+
+  // Typography
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'normal',
+  },
+  bodyText: {
+    fontSize: 16,
+    fontWeight: 'normal',
+  },
+
+  // Spacing
+  margin: {
+    margin: 10,
+  },
+  padding: {
+    padding: 10,
+  },
+});
+
+export default styles;
+```
+
