36ab8adbff0283811f8ff1c2d18e391da4d5f6fd
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-02-10 23:34:33 +0100

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,27 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
-import HomeScreen from './src/screens/HomeScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import { DeepLink, Linking } from 'expo';
+
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import VideoScreen from './screens/VideoScreen';
+import SearchScreen from './screens/SearchScreen';
+import UploadScreen from './screens/UploadScreen';
 
 const Stack = createStackNavigator();
 
-type RootStackParamList = {
-  Home: undefined;
-  Profile: { username: string };
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Video" component={VideoScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Upload" component={UploadScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-export default function App() {
-  const [initialNavigationState, setInitialNavigationState] = useState<any>();
-  const [isReady, setIsReady] = useState(false);
-  const deepLink = Linking.createURL('/profile/username');
-
-  useEffect(() => {
-    Linking.addEventListener('url', handleDeepLink);
-
-    return () => {
-      Linking.removeEventListener('url', handleDeepLink);
-    };
-  }, []);
-
-  const handleDeepLink = (event: any) => {
-    const data = event.url;
-    if (data) {
-      setInitialNavigationState({
-        routes: [
-          {
-            name: 'Profile',
-            params: {
-              username: data.split('/')[2],
-            },
-          },
-        ],
-      });
-    }
-  };
-
-  if (isReady) {
-    return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <NavigationContainer initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Profile"
-              component={ProfileScreen}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </>
-    );
-  } else {
-    return null;
-  }
-}
+export default AppNavigation;
